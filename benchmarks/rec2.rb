ca723e9c4f6707f5843e98d80ce94cb60393c3d5
Joshua Hull <joshbuddy@gmail.com>
2010-06-28 21:25:07 -0400

    smaller set of benchmarks to match rack-mount

--- benchmarks/rec2.rb
+++ benchmarks/rec2.rb
@@ -5,31 +5,31 @@ require 'lib/http_router'
 u = HttpRouter.new
 u.add('/simple').to {|env| [200, {'Content-type'=>'text/html'}, []]}
 u.add('/simple/again').compile.to {|env| [200, {'Content-type'=>'text/html'}, []]}
-u.add('/simple/again/and/again').compile.to {|env| [200, {'Content-type'=>'text/html'}, []]}
+#u.add('/simple/again/and/again').compile.to {|env| [200, {'Content-type'=>'text/html'}, []]}
 u.add('/dynamic/:variable').compile.to {|env| [200, {'Content-type'=>'text/html'}, []]}
-u.add('/rails/:controller/:action/:id').compile.to {|env| [200, {'Content-type'=>'text/html'}, []]}
-u.add('/greedy/:greed').matching(:greed => /.*/).compile.to {|env| [200, {'Content-type'=>'text/html'}, []]}
+#u.add('/rails/:controller/:action/:id').compile.to {|env| [200, {'Content-type'=>'text/html'}, []]}
+#u.add('/greedy/:greed').matching(:greed => /.*/).compile.to {|env| [200, {'Content-type'=>'text/html'}, []]}
 #u.add('/greedy/hey.:greed.html').to {|env| [200, {'Content-type'=>'text/html'}, []]}
 
-#puts Benchmark.measure {
-#  ('aa'..'nn').each do |first|
-#    ('a'..'n').each do |second|
-#      u.add("/#{first}/#{second}").compile
-#    end
-#  end
+puts Benchmark.measure {
+  ('aa'..'nn').each do |first|
+    ('a'..'n').each do |second|
+      u.add("/#{first}/#{second}").to {|env| [200, {'Content-type'=>'text/html'}, []]}
+    end
+  end
 #
-#  puts "u.routes.size: #{u.routes.size}"
-#}
+  puts "u.routes.size: #{u.routes.size}"
+}
 #
 TIMES = 50_000
 
 simple_env = Rack::MockRequest.env_for('/simple')
 simple2_env = Rack::MockRequest.env_for('/simple/again')
-simple3_env = Rack::MockRequest.env_for('/simple/again/and/again')
+#simple3_env = Rack::MockRequest.env_for('/simple/again/and/again')
 simple_and_dynamic_env = Rack::MockRequest.env_for('/dynamic/anything')
-simple_and_dynamic_env1 = Rack::MockRequest.env_for('/rails/controller/action/id')
-simple_and_dynamic_env2 = Rack::MockRequest.env_for('/greedy/controller/action/id')
-simple_and_dynamic_env3 = Rack::MockRequest.env_for('/greedy/hey.hello.html')
+#simple_and_dynamic_env1 = Rack::MockRequest.env_for('/rails/controller/action/id')
+#simple_and_dynamic_env2 = Rack::MockRequest.env_for('/greedy/controller/action/id')
+#simple_and_dynamic_env3 = Rack::MockRequest.env_for('/greedy/hey.hello.html')
 
 RBench.run(TIMES) do
 
@@ -41,21 +41,21 @@ RBench.run(TIMES) do
     u.call(simple2_env).first == 200 or raise
   end
 
-  report "8 levels, static" do
-    u.call(simple3_env).first == 200 or raise
-  end
+  #report "8 levels, static" do
+  #  u.call(simple3_env).first == 200 or raise
+  #end
 
   report "4 levels, 1 dynamic" do
     u.call(simple_and_dynamic_env).first == 200 or raise
   end
 
-  report "8 levels, 3 dynamic" do
-    u.call(simple_and_dynamic_env1).first == 200 or raise
-  end
-
-  report "4 levels, 1 greedy" do
-    u.call(simple_and_dynamic_env2).first == 200 or raise
-  end
+  #report "8 levels, 3 dynamic" do
+  #  u.call(simple_and_dynamic_env1).first == 200 or raise
+  #end
+  #
+  #report "4 levels, 1 greedy" do
+  #  u.call(simple_and_dynamic_env2).first == 200 or raise
+  #end
 
 end
 
