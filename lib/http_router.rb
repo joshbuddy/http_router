4ac1ce3f7769b174ce50219b294754e1fa78ff77
Josh Hull <joshbuddy@twitter.com>
2011-03-23 00:50:48 -0700

    re-add docs

--- lib/http_router.rb
+++ lib/http_router.rb
@@ -13,8 +13,11 @@ class HttpRouter
   attr_reader :root, :routes, :known_methods, :named_routes
   attr_accessor :default_app, :url_mount
 
+  # Raised when a Route is not able to be generated.
   UngeneratableRouteException = Class.new(RuntimeError)
+  # Raised when a Route is generated that isn't valid.
   InvalidRouteException       = Class.new(RuntimeError)
+  # Raised when a Route is not able to be generated due to a missing parameter.
   MissingParameterException   = Class.new(RuntimeError)
 
   # Creates a new HttpRouter.
@@ -34,6 +37,21 @@ class HttpRouter
     instance_eval(&blk) if blk
   end
 
+  # Adds a path to be recognized.
+  #
+  # To assign a part of the path to a specific variable, use :variable_name within the route.
+  # For example, <tt>add('/path/:id')</tt> would match <tt>/path/test</tt>, with the variable <tt>:id</tt> having the value <tt>"test"</tt>.
+  #
+  # You can receive mulitple parts into a single variable by using the glob syntax.
+  # For example, <tt>add('/path/*id')</tt> would match <tt>/path/123/456/789</tt>, with the variable <tt>:id</tt> having the value <tt>["123", "456", "789"]</tt>.
+  #
+  # As well, paths can end with two optional parts, <tt>*</tt> and <tt>/?</tt>. If it ends with a <tt>*</tt>, it will match partially, returning the part of the path unmatched in the PATH_INFO value of the env. The part matched to will be returned in the SCRIPT_NAME. If it ends with <tt>/?</tt>, then a trailing / on the path will be optionally matched for that specific route. As trailing /'s are ignored by default, you probably don't actually want to use this option that frequently.
+  #
+  # Routes can also contain optional parts. There are surrounded with <tt>( )</tt>'s. If you need to match on a bracket in the route itself, you can escape the parentheses with a backslash.
+  #
+  # The second argument, options, is an optional hash that can modify the route in further ways. See HttpRouter::Route#with_options for details. Typically, you want to add further options to the route by calling additional methods on it. See HttpRouter::Route for further details.
+  #
+  # Returns the route object.
   def add(path, opts = {}, &app)
     route = case path
     when Regexp
@@ -50,16 +68,38 @@ class HttpRouter
     @routes << route
   end
 
-  def post(path, opts = {}, &app);   add_with_request_method(path, :post, opts, &app);   end
+  # Adds a path that only responds to the request method +GET+.
+  #
+  # Returns the route object.
   def get(path, opts = {}, &app);    add_with_request_method(path, :get, opts, &app);    end
+
+  # Adds a path that only responds to the request method +POST+.
+  #
+  # Returns the route object.
+  def post(path, opts = {}, &app);   add_with_request_method(path, :post, opts, &app);   end
+
+  # Adds a path that only responds to the request method +HEAD+.
+  #
+  # Returns the route object.
   def head(path, opts = {}, &app);   add_with_request_method(path, :head, opts, &app);   end
+
+  # Adds a path that only responds to the request method +DELETE+.
+  #
+  # Returns the route object.
   def delete(path, opts = {}, &app); add_with_request_method(path, :delete, opts, &app); end
+
+  # Adds a path that only responds to the request method +PUT+.
+  #
+  # Returns the route object.
   def put(path, opts = {}, &app);    add_with_request_method(path, :put, opts, &app);    end
 
   def recognize(env)
     call(env, false)
   end
 
+  # Rack compatible #call. If matching route is found, and +dest+ value responds to #call, processing will pass to the matched route. Otherwise,
+  # the default application will be called. The router will be available in the env under the key <tt>router</tt>. And parameters matched will
+  # be available under the key <tt>router.params</tt>.
   def call(env, perform_call = true)
     rack_request = Rack::Request.new(env)
     if redirect_trailing_slash? && (rack_request.head? || rack_request.get?) && rack_request.path_info[-1] == ?/
@@ -86,6 +126,7 @@ class HttpRouter
     end
   end
 
+  # Resets the router to a clean state.
   def reset!
     @root = Node.new(self)
     @default_app = Proc.new{ |env| Rack::Response.new("Your request couldn't be found", 404).finish }
@@ -94,6 +135,25 @@ class HttpRouter
     @known_methods = ['GET', "POST", "PUT", "DELETE"]
   end
 
+  # Assigns the default application.
+  def default(app)
+    @default_app = app
+  end
+
+  # Generate a URL for a specified route. This will accept a list of variable values plus any other variable names named as a hash.
+  # This first value must be either the Route object or the name of the route.
+  #
+  # Example:
+  #   router = HttpRouter.new
+  #   router.add('/:foo.:format).name(:test).compile
+  #   router.url(:test, 123, 'html')
+  #   # ==> "/123.html"
+  #   router.url(:test, 123, :format => 'html')
+  #   # ==> "/123.html"
+  #   router.url(:test, :foo => 123, :format => 'html')
+  #   # ==> "/123.html"
+  #   router.url(:test, :foo => 123, :format => 'html', :fun => 'inthesun')
+  #   # ==> "/123.html?fun=inthesun"
   def url(route, *args)
     case route
     when Symbol then @named_routes.key?(route) ? @named_routes[route].url(*args) : raise(UngeneratableRouteException)
@@ -102,10 +162,12 @@ class HttpRouter
     end
   end
 
+  # Ignore trailing slash feature enabled? See #initialize for details.
   def ignore_trailing_slash?
     @ignore_trailing_slash
   end
 
+  # Redirect trailing slash feature enabled? See #initialize for details.
   def redirect_trailing_slash?
     @redirect_trailing_slash
   end
