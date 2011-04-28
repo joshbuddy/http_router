f5e320cf25142098c90b827aca7863e5e1a2b86e
Josh Hull <joshbuddy@twitter.com>
2011-04-28 10:07:53 -0700

    use node_lookup alias

--- lib/http_router/node/arbitrary.rb
+++ lib/http_router/node/arbitrary.rb
@@ -1,6 +1,8 @@
 class HttpRouter
   class Node
     class Arbitrary < Node
+      alias_method :node_lookup, :[]
+
       def initialize(router, allow_partial, blk, param_names)
         @allow_partial, @blk, @param_names = allow_partial, blk, param_names
         super(router)
@@ -10,7 +12,7 @@ class HttpRouter
         if request.path.empty? or (request.path.size == 1 and request.path[0] == '') or @allow_partial
           catch(:pass) do
             request = request.clone
-            request.continue = proc { |state| super(request) if state }
+            request.continue = proc { |state| node_lookup(request) if state }
             params = @param_names.nil? ? {} : Hash[@param_names.zip(request.params)]
             @blk.call(request, params)
           end
