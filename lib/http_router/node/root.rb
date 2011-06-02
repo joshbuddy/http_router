899b1645cb471dd0a0f71a7c7ae304e803ed24cf
Josh Hull <joshbuddy@twitter.com>
2011-06-01 22:29:14 -0700

    even cleaner

--- lib/http_router/node/root.rb
+++ lib/http_router/node/root.rb
@@ -1,8 +1,10 @@
 class HttpRouter
   class Node
     class Root < Node
+      attr_reader :methods_module
       def initialize(router)
         super(router, nil)
+        @methods_module = Module.new
       end
 
       def [](request)
@@ -10,13 +12,10 @@ class HttpRouter
         self[request]
       end
 
+      private
       def compile
         root.extend(root.methods_module)
         instance_eval "def [](request)\n#{to_code}\nnil\nend", __FILE__, __LINE__
-      end
-
-      def methods_module
-        @module ||= Module.new
       end
     end
   end
