1266be380977bbb38f7bff871f2d248cd956834c
Josh Hull <joshbuddy@twitter.com>
2011-06-09 09:37:23 -0700

    mildly tidier lookup

--- lib/http_router/node/lookup.rb
+++ lib/http_router/node/lookup.rb
@@ -17,15 +17,16 @@ class HttpRouter
       def to_code
         lookup_ivar = :"@lookup_#{router.next_counter}"
         inject_root_ivar(lookup_ivar, @map)
+        method_prefix = "lookup_#{router.next_counter} "
         inject_root_methods @map.keys.map {|k| 
-          method = :"lookup_#{object_id} #{k}"
+          method = :"#{method_prefix}#{k}"
           "define_method(#{method.inspect}) do |request|
             part = request.path.shift
             #{@map[k].map{|n| n.to_code} * "\n"}
             request.path.unshift part
           end"}.join("\n")
         code = "
-        send(\"lookup_#{object_id} \#{request.path.first}\", request) if !request.path_finished? && #{lookup_ivar}.key?(request.path.first)
+        send(\"#{method_prefix}\#{request.path.first}\", request) if !request.path_finished? && #{lookup_ivar}.key?(request.path.first)
         "
       end
     end
